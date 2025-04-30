84f8993df59207215b2abdbd1caa2805dc464460
hge2ne <hge2ne@gmial.com>
2025-05-01 00:11:13 +0900

    초기 설정 완료

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+
+ let post = '강남 우동 맛집';
+
+  return ( /* 주의: 리턴() 안에 병렬로 태그 2개 이상 기입 금지 */
+      <div className="App">
+        <div className="black-nav">
+          <h4>블로그임</h4>
+        </div>
+        <div className="list">
+            <h4>글제목</h4>
+            <p>2월 17일 발행</p>
+          </div>
+        </div>
+      
+  );
+}
+
+export default App
